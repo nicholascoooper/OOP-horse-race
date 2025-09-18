b58fbd9cdee62f65cf428c26a8a34fa7f6c9dfb3
nicholascoooper <nicholascoopernsc@gmail.com>
2025-09-18 19:08:36 +0000

    comment

---
+++ horse.cpp
@@ -0,0 +1,18 @@
+#ifndef HORSE_H_EXIST
+#define HORSE_H_EXIST
+
+class Horse{
+	private:
+		int position;
+		int id;
+		int trackLength;
+
+	public:
+		Horse();
+		void init(int id, int trackLength);
+		void advance();
+		void printLane();
+		void isWinner();
+};
+
+#endif
